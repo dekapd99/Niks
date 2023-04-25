89d188c9daa7f033c23d331be2ee5bc8e166db45
dekapd99 <dekaprimatio@gmail.com>
2023-04-25 10:51:13 +0700

    Initial Commit

---
+++ Niks/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Niks
+//
+//  Created by Deka Primatio on 25/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Niks/NiksApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiksApp.swift
+//  Niks
+//
+//  Created by Deka Primatio on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct NiksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
