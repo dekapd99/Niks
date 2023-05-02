d875d6dd1fad3bd080a155ac5b28d1f014bbbe66
- <->
2023-05-02 19:08:43 +0700

    - Modified Timer Component 	- added diameter and wideness parameter - Modified Button Component 	- change content of button - Added StretchingView

--- Niks/View/StretchingView.swift
+++ Niks/View/StretchingView.swift
@@ -9,7 +9,47 @@ import SwiftUI
 
 struct StretchingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        @State var toggle: Bool = false
+        GeometryReader { geometry in
+            ZStack{
+                Image(Constant.BackgroundImage.BackgroundStretch)
+                VStack {
+                    HStack {
+                        Spacer()
+                        Image("StretchTitle")
+                            .resizable()
+                            .frame(width: geometry.size.width * 0.24, height: geometry.size.height * 0.15)
+                            
+                    }
+                    .padding(.bottom, geometry.size.height * 0.05)
+                    HStack {
+                        Spacer()
+                        TimerComponent(timer: 60,
+                                       colorgrad: Constant.ColorStyle.Purple,
+                                       diameter: geometry.size.width * 0.1,
+                                       wideness: geometry.size.width * 0.02)
+                            .padding(.trailing, geometry.size.width * 0.025)
+                    }
+                    Spacer()
+                }
+                BubbleDialogueComponent()
+                    .position(x: geometry.size.width / 2, y: geometry.size.height * 0.12)
+                HStack{
+                    Spacer()
+                    ButtonComponent(
+                        rounded: true,
+                        colorgrad: Constant.ColorStyle.DarkPurple,
+                        doThis: {})
+//                    .frame(width: geometry.size.width * 0.1, height: geometry.size.height * 0.15)
+                }
+                .padding()
+                
+                
+            }
+            .ignoresSafeArea()
+            .position(x: geometry.size.width/2, y: geometry.size.height/2)
+        }
+        
     }
 }
 
