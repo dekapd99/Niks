d3723f7cabc578eb1ab05827b2d8d445949843ee
- <->
2023-05-06 13:28:12 +0700

    - Fixed AnimationView - Updated PreviewStretch, StretchingView, & TimerComponent

--- Niks/Component/TimerComponent.swift
+++ Niks/Component/TimerComponent.swift
@@ -12,15 +12,13 @@ struct TimerComponent: View {
     let colorgrad: Color
     let diameter: CGFloat
     let wideness: CGFloat
-    let isFinish: () -> ()
-    @Binding var isActive: Bool
+    @ObservedObject var viewModel: AnimatorViewModel
     var body: some View {
         CircularProgressBar(diameter: diameter,
                             wideness: wideness,
                             timer: timer,
                             colorgrad: colorgrad,
-                            isActive: $isActive,
-                            isFinish: isFinish)
+                            viewModel: viewModel)
     }
 }
 
@@ -30,20 +28,16 @@ struct CircularProgressBar: View {
     let wideness: CGFloat
     let fontgrad: Font = .title
     var fontsize: CGFloat {return wideness - 10}
-    let opt: Int = 0
     let shadowOpac: CGFloat = 0.4
     var colorPrimary: Color {return colorgrad.opacity(0.3)}
     var colorSecondary: Color {return colorgrad}
-    @State var currentTime: CGFloat = 0
     @State var timer: CGFloat
     @State var opacTimer: CGFloat = 5
     let colorgrad: Color
-    @Binding var isActive: Bool
-    let isFinish: () -> ()
+    @State var done: Bool = false
+    @ObservedObject var viewModel: AnimatorViewModel
+    var opt: Int {return viewModel.opt}
     var body: some View {
-        let TimerObj = Timer.publish(every: 1,
-                                     on: .main,
-                                     in: .common).autoconnect()
         ZStack {
             Circle()
                 .stroke(
@@ -64,50 +58,34 @@ struct CircularProgressBar: View {
                 .opacity(getOpacTimer())
                 .rotationEffect(.degrees(-90))
                 .shadow(color: .gray.opacity(shadowOpac), radius: 30, x: 0, y: 0.5)
-                .animation(.easeInOut(duration: smoothness), value: currentTime)
-            Text("\(Int(currentTime))s")
+                .animation(.easeInOut(duration: smoothness), value: viewModel.currentTime)
+            Text("\(Int(viewModel.currentTime))s")
                 .titleStyle()
-                .opacity(currentTime == 0 ? 0.3 : 0.8)
+                .opacity(viewModel.currentTime == 0 ? 0.3 : 0.8)
                 .foregroundColor(colorSecondary)
         }
-        .onAppear(perform: {currentTime = getInitialVal()})
-        .onReceive(TimerObj, perform: { _ in getOperation()})
+        .onAppear(perform: {viewModel.currentTime = getInitialVal()})
+//        .onReceive(TimerObj, perform: { _ in
+//            if currentTime == 0 && !done{
+//                isFinish()
+//                done = true
+//            }
+//        })
         .frame(width: diameter,
                height: diameter,
                alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
     }
     func getProgress() -> CGFloat {
-        return currentTime/timer < 0 ? 0 : currentTime/timer
+        return viewModel.currentTime/timer < 0 ? 0 : viewModel.currentTime/timer
     }
-    func decrementTime() -> () {
-        if isActive {
-            if currentTime == 0 {
-                isFinish()
-            }
-            currentTime = currentTime <= 0 ? 0 : currentTime - 1
-        }
-    }
-    func incrementTime() -> () {
-        if isActive {
-            currentTime = currentTime >= timer ? timer : currentTime + 1
-        }
-    }
+    
     func getText() -> String {
         guard opt >= 0 || opt < 2 else { return "" }
         if opt == 1 {
             return "\(Int((getProgress()*100).rounded(.up)))%"
         }else {
-            return "00:\(Int(currentTime.rounded(.up)) < 10 ? "0" : "")\(Int(currentTime.rounded(.up)))"
+            return "00:\(Int(viewModel.currentTime.rounded(.up)) < 10 ? "0" : "")\(Int(viewModel.currentTime.rounded(.up)))"
         }
-    }
-    func getOperation() -> () {
-        guard opt >= 0 || opt < 2 else { return }
-        if opt == 1 {
-            incrementTime()
-        }else {
-            decrementTime()
-        }
-        
     }
     func getInitialVal() -> CGFloat {
         guard opt >= 0 || opt < 2 else { return timer }
@@ -118,7 +96,7 @@ struct CircularProgressBar: View {
         }
     }
     func getOpacTimer() -> CGFloat {
-        return currentTime == 0 ? 0 : 1
+        return viewModel.currentTime == 0 ? 0 : 1
     }
 }
 
